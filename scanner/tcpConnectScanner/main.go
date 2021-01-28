56896835e51bd4dc15db468d8c7c62c813fd8881
林禹 <yl574300833@outlook.com>
2021-01-28 23:50:44 +0800

    update TCP Tool

--- scanner/tcpConnectScanner/main.go
+++ scanner/tcpConnectScanner/main.go
@@ -2,46 +2,43 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"myTools/scanner/tcpConnectScanner/iputil"
+	"myTools/scanner/iputil"
+	"myTools/scanner/task"
 	"myTools/scanner/tcpConnectScanner/tcpScannerTool"
+	"os"
+	"runtime"
 )
 
 func main() {
 
-	fmt.Printf("0: %s\n",os.Args[0])
-	fmt.Printf("1: %s\n",os.Args[1])
-	fmt.Printf("2: %s\n",os.Args[2])
-
 	if len(os.Args) == 3 {
 		ipList := os.Args[1]
 		portList := os.Args[2]
+
+		iputil := new(iputil.DefaultIputil)
+		task := &task.DefaultTask{
+			TcpTool: tcpScannerTool.DefaultTcpTool{},
+		}
+
 		ips, err := iputil.GetIpList(ipList)
 		if err != nil {
 			fmt.Printf("parse ip list error: %+v\n", err)
 		}
-		fmt.Println(ips)
 		ports, err := iputil.GetPorts(portList)
 
 		if err != nil {
 			fmt.Printf("parse ports error: %+v\n", err)
 		}
-		fmt.Println(ports)
 
-		for _, ipItem := range ips {
-			for _, portItem := range ports {
-				_, err := tcpScannerTool.Connect(ipItem.String(),portItem)
-				if err != nil {
-					continue
-				}
-				fmt.Printf("ip: %v, port: %v is open\n", ipItem, portItem)
-			}
-		}
+		tasks, _ := task.GenerateTask(ips, ports)
+		task.RunTask(tasks)
+		task.PrintResult()
 	} else {
 		fmt.Printf("%v iplist port\n", os.Args[0])
 	}
 }
 
-
 // go run .\main.go 45.22.2.156,114.114.114.114 22,23,24,25,80-100
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
--- scanner/tcpConnectScanner/tcpScannerTool/tcpScanner.go
+++ scanner/tcpConnectScanner/tcpScannerTool/tcpScanner.go
@@ -6,13 +6,21 @@ import (
 	"time"
 )
 
-func Connect(ip string, port int) (net.Conn, error) {
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%v", ip, port), 1*time.Second)
+type MyTcpTool interface {
+	MyConnect(ip string, port int) (string, int, error)
+}
+
+type DefaultTcpTool struct {}
+
+func (tcpTool DefaultTcpTool) MyConnect(ip string, port int) (string, int, error) {
+
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%v", ip, port), 2*time.Second)
 
 	defer func() {
 		if conn != nil {
 			_ = conn.Close()
 		}
 	}()
-	return conn, err
+
+	return ip, port, err
 }
